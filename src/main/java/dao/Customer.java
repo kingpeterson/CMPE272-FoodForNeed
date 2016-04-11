6a296bcf7aff1cd602420ebd83eb29f7af861845
blueMac <bluebluebluelan@gmail.com>
2016-04-11 11:09:06 +0900

    add 3 table

---
+++ src/main/java/dao/Customer.java
@@ -0,0 +1,160 @@
+package dao;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+/*
+ * define O-R mapping of customer table
+ */
+public class Customer {
+	@Id //primary key
+	@Column(name = "customerId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+//	customerId, customerName, city, phone, latitude, longitude, addressLine1, state, country, postalCode
+	
+	@Basic
+	@Column(name = "customerName")
+	String name;
+	
+	@Basic
+	@Column(name = "city")
+	String city;
+	
+	@Basic
+	@Column(name = "phone")
+	String phone;
+	
+	@Basic
+	@Column(name = "latitude")
+	Double latitude;
+	
+	@Basic
+	@Column(name = "longitude")
+	Double longitude;
+	
+	@Basic
+	@Column(name = "addressLine1")
+	String addressLine1;
+	
+	@Basic
+	@Column(name = "addressLine2")
+	String addressLine2;
+	
+	@Basic
+	@Column(name = "state")
+	String state;
+	
+	@Basic
+	@Column(name = "country")
+	String country;
+	
+	@Basic
+	@Column(name = "postalCode")
+	String postalCode;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long pk) {
+		id = pk;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	
+	public String getAddressline1() {
+		return addressLine1;
+	}
+
+	public void setAddressline1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	
+	public String getAddressline2() {
+		return addressLine2;
+	}
+
+	public void setAddressline2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getPostalcode() {
+		return postalCode;
+	}
+
+	public void setPostalcode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("{\"id\": \"%d\", \"name\": \"%s\", \"city\": \"%s\", "
+				+ "\"phone\": \"%s\", \"latitude\": \"%f\", \"longitude\": \"%f\", "
+				+ "\"addressLine1\": \"%s\", \"addressLine2\": \"%s\", \"state\": \"%s\", "
+				+ "\"country\": \"%s\", \"postalCode\": \"%s\"}", 
+				id, name, city, phone, latitude, longitude, addressLine1, addressLine2, state, country, postalCode);
+	}
+}
---
+++ src/main/java/dao/Product.java
@@ -0,0 +1,113 @@
+package dao;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+/*
+ * define O-R mapping of product table
+ */
+public class Product {
+	@Id //primary key
+	@Column(name = "productId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	
+//	productId, productName, productLine, productScale, latitude, productVendor, productDescription
+	
+	@Basic
+	@Column(name = "productName")
+	String name;
+	
+	@Basic
+	@Column(name = "productLine")
+	String productLine;
+	
+	@Basic
+	@Column(name = "productScale")
+	String productScale;
+
+	@Basic
+	@Column(name = "productVendor")
+	Int productVendor;
+	
+	@Basic
+	@Column(name = "productDescription")
+	String productDescription;
+	
+	@Basic
+	@Column(name = "quantityInStock")
+	String quantityInStock;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long pk) {
+		id = pk;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getProductLine() {
+		return productLine;
+	}
+
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+	
+	public String getProductScale() {
+		return productScale;
+	}
+
+	public void setProductScale(String productScale) {
+		this.productScale = productScale;
+	}
+	
+	public Int getProductVendor() {
+		return productVendor;
+	}
+
+	public void setProductVendor(Double productVendor) {
+		this.productVendor = productVendor;
+	}
+	
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	public Int getQuantityInStock() {
+		return quantityInStock;
+	}
+
+	public void setQuantityInStock(String quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+	
+
+
+	@Override
+	public String toString() {
+		return String.format("{\"id\": \"%d\", \"name\": \"%s\", \"productLine\": \"%s\", "
+				+ "\"productScale\": \"%s\", \"latitude\": \"%f\", \"productVendor\": \"%d\", "
+				+ "\"productDescription\": \"%s\", \"quantityInStock\": \"%d\"}", 
+				id, name, productLine, productScale, latitude, productVendor, productDescription, quantityInStock);
+	}
+}
