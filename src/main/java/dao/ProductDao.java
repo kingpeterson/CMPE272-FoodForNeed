302574d202366e3ddf9a645fe8e1abd716ee5531
Peter Wang <kingpetersonwang@gmail.com>
2016-04-20 16:23:04 -0700

    fix donate.html table

---
+++ src/main/java/dao/ProductDao.java
@@ -0,0 +1,248 @@
+package dao;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+// CRUD API at /api/todolist
+@Path("/productDao")
+/**
+ * RESTful CRUD service of database.
+ *
+ */
+public class ProductDao {
+
+	private UserTransaction utx;
+	private EntityManager em;
+
+	public ProductDao() {
+		utx = getUserTransaction();
+		em = getEm();
+	}
+
+	@POST
+	public Response create(@FormParam("name") String name) {
+		
+		Product product = new Product();
+		product.setName(name);
+		try {
+			utx.begin();
+			em.persist(product);
+			utx.commit();
+			return Response.ok(product.toString()).build();
+		} catch (Exception e) {
+			e.printStackTrace();			
+			return Response.status(javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			try {
+				if (utx.getStatus() == javax.transaction.Status.STATUS_ACTIVE) {
+					utx.rollback();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@DELETE
+	public Response delete(@QueryParam("id") long id) {
+		
+		try {
+			utx.begin();
+			Product product = em.find(Product.class, id);
+			if (product != null) {
+				em.remove(product);
+				utx.commit();
+				return Response.ok().build();
+			} else {
+				return Response.status(javax.ws.rs.core.Response.Status.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			try {
+				if (utx.getStatus() == javax.transaction.Status.STATUS_ACTIVE) {
+					utx.rollback();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@PUT
+	public Response update(@FormParam("id") long id, @FormParam("name") String name) {
+		
+		try {
+			utx.begin();
+			Product product = em.find(Product.class, id);
+			if (product != null) {
+				product.setName(name);// TODO check if null
+				em.merge(product);
+				utx.commit();
+				return Response.ok().build();
+			} else {
+				return Response.status(javax.ws.rs.core.Response.Status.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			try {
+				if (utx.getStatus() == javax.transaction.Status.STATUS_ACTIVE) {
+					utx.rollback();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get(@QueryParam("id") long id) {
+		if (id == 0) {
+			List<Product> list = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+			//TODO use JSON util like Gson to render objects and use REST Response Writer
+			String json = "{\"id\":\"all\", \"body\":" + list.toString() + "}";
+			System.out.println("I am at id == 0");
+			System.out.println(json);
+			return Response.ok(json).build();
+		}
+		Product product = null;
+		try {
+			utx.begin();
+			product = em.find(Product.class, id);
+			utx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			try {
+				if (utx.getStatus() == javax.transaction.Status.STATUS_ACTIVE) {
+					utx.rollback();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (product != null){
+			System.out.println("I am at found");
+			System.out.println(product.toString());
+			return Response.ok(product.toString()).build();
+		}
+		else
+			return Response.status(javax.ws.rs.core.Response.Status.NOT_FOUND).build();
+	}
+	
+	
+	/*
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String get(@QueryParam("id") long id) {
+		if (id == 0) {
+			List<Customer> list = em.createQuery("SELECT t FROM Customers t", Customer.class).getResultList();
+			//TODO use JSON util like Gson to render objects and use REST Response Writer
+			String json = "{\"id\":\"all\", \"body\":" + list.toString() + "}";
+			System.out.println("I am at id == 0");
+			System.out.println(json);
+			return json;
+		}
+		Customer customer = null;
+		try {
+			utx.begin();
+			customer = em.find(Customer.class, id);
+			utx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "INTERNAL SERVER ERROR";
+		} finally {
+			try {
+				if (utx.getStatus() == javax.transaction.Status.STATUS_ACTIVE) {
+					utx.rollback();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (customer != null){
+			System.out.println("I am at found");
+			System.out.println(customer.toString());
+			return customer.toString();
+		}
+		else
+			return "NOT FOUND";
+	}
+	*/
+	/*
+	public void populateDB() {
+		List<TODO> list = em.createQuery("SELECT t FROM TODO t", TODO.class).getResultList();
+		if (list.size() == 0) {
+			create("sample entry #1");
+			create("sample entry #2");
+			create("sample entry #3");
+		}
+	}
+	*/
+	private UserTransaction getUserTransaction() {
+		InitialContext ic;
+		try {
+			ic = new InitialContext();
+			return (UserTransaction) ic.lookup("java:comp/UserTransaction");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// There are two ways of obtaining the connection information for some services in Java 
+	
+	// Method 1: Auto-configuration and JNDI
+	// The Liberty buildpack automatically generates server.xml configuration 
+	// stanzas for the SQL Database service which contain the credentials needed to 
+	// connect to the service. The buildpack generates a JNDI name following  
+	// the convention of "jdbc/<service_name>" where the <service_name> is the 
+	// name of the bound service. 
+	// Below we'll do a JNDI lookup for the EntityManager whose persistence 
+	// context is defined in web.xml. It references a persistence unit defined 
+	// in persistence.xml. In these XML files you'll see the "jdbc/<service name>"
+	// JNDI name used.
+
+	private EntityManager getEm() {
+		InitialContext ic;
+		try {
+			ic = new InitialContext();
+			return (EntityManager) ic.lookup("java:comp/env/openjpa-todo/entitymanager");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	// Method 2: Parsing VCAP_SERVICES environment variable
+    // The VCAP_SERVICES environment variable contains all the credentials of 
+	// services bound to this application. You can parse it to obtain the information 
+	// needed to connect to the SQL Database service. SQL Database is a service
+	// that the Liberty buildpack auto-configures as described above, so parsing
+	// VCAP_SERVICES is not a best practice.
+
+	// see HelloResource.getInformation() for an example
+
+}
---
+++ src/main/webapp/donate.js
@@ -0,0 +1,147 @@
+var MARKET_DATA = 'api//marketDao';
+var PRODUCT_DATA = 'api//productDao';
+
+function loadMarket(){
+	xhrGet(MARKET_DATA, function(data){
+		var receivedItems = data.body || [];
+		var items = [];
+		var i;		
+		// Make sure the received items have correct format
+		console.log(receivedItems);
+		for(i = 0; i < receivedItems.length; ++i){
+			var item = receivedItems[i];
+			if(item && 'id' in item && 'name' in item){
+				items.push(item);
+			}
+		}
+		for(i = 0; i < items.length; ++i){
+			console.log(items[i]);
+			addMarket(items[i], false);
+
+		}
+	}, function(err){
+		console.error(err);
+	});
+}
+
+function loadProduct(){
+	xhrGet(PRODUCT_DATA, function(data){
+		var receivedItems = data.body || [];
+		var items = [];
+		var i;		
+		// Make sure the received items have correct format
+		console.log(receivedItems);
+		for(i = 0; i < receivedItems.length; ++i){
+			var item = receivedItems[i];
+			if(item && 'id' in item && 'name' in item){
+				items.push(item);
+			}
+		}
+		for(i = 0; i < items.length; ++i){
+			console.log(items[i]);
+			addMarket(items[i], false);
+
+		}
+	}, function(err){
+		console.error(err);
+	});
+}
+/*
+function addItem(item, isNew){
+	var row = document.createElement('tr');
+	var id = item && item.id;
+	if(id){
+		row.setAttribute('data-id', id);
+	}
+	row.innerHTML = "<td style='width:90%'><textarea onchange='saveChange(this)' onkeydown='onKey(event)'></textarea></td>" +
+		"<td class='deleteBtn' onclick='deleteItem(this)' title='delete me'></td>";
+	var table = document.getElementById('marketList');
+	console.log(table.lastChild);
+	table.lastChild.appendChild(row);
+	var textarea = row.firstChild.firstChild;
+	if(item){
+		textarea.value = item.name;
+	}
+	row.isNew = !item || isNew;
+	textarea.focus();
+}
+*/
+function addMarket(item, isNew){
+	var row = document.createElement('tr');
+	row.setAttribute('class', 'gradeA');	
+	var id = item && item.id;
+	if(id){
+		row.setAttribute('data-id', id);
+	}
+	row.innerHTML = "<td id=marketName"+id+"></td>" +
+					"<td id=marketPhone"+id+"></td>"+
+					"<td id=marketAddress"+id+"></td>";
+//    				"<button class='deleteBtn' onclick='deleteItem(this)' title='delete me'></button>";
+	var table = document.getElementById('marketList');
+//	var table = document.getElementById('marketList').getElementsByTagName('tbody')[0];
+//	// Insert a row in the table at the last row
+//	var newRow   = tableRef.insertRow(tableRef.rows.length);
+//
+//	// Insert a cell in the row at index 0
+//	var newCell  = newRow.insertCell(0);
+	table.tBodies[0].appendChild(row);
+
+
+//	table.appendChild(row);
+	
+	if(item){
+		document.getElementById('marketName'+id).innerHTML = item.name;
+		document.getElementById('marketPhone'+id).innerHTML = item.phone;
+		document.getElementById('marketAddress'+id).innerHTML = item.addressLine1 + ", " + item.city + " " + item.state +
+		" " + item.postalCode;
+	}
+
+	row.isNew = !item || isNew;
+}
+
+function deleteItem(deleteBtnNode){
+	var row = deleteBtnNode.parentNode;
+	row.parentNode.removeChild(row);
+	xhrDelete(REST_DATA + '?id=' + row.getAttribute('data-id'), function(){
+	}, function(err){
+		console.error(err);
+	});
+}
+
+function onKey(evt){
+	if(evt.keyCode == KEY_ENTER && !evt.shiftKey){
+		evt.stopPropagation();
+		evt.preventDefault();
+		var row = evt.target.parentNode.parentNode;
+		if(row.nextSibling){
+			row.nextSibling.firstChild.firstChild.focus();
+		}else{
+			addItem();
+		}
+	}
+}
+
+function saveChange(contentNode, callback){
+	var row = contentNode.parentNode.parentNode;
+	var data = {
+		name: contentNode.value
+	};
+	if(row.isNew){
+		delete row.isNew;
+		xhrPost(REST_DATA, data, function(item){
+			row.setAttribute('data-id', item.id);
+			callback && callback();
+		}, function(err){
+			console.error(err);
+		});
+	}else{
+		data.id = row.getAttribute('data-id');
+		xhrPut(REST_DATA, data, function(){
+			console.log('updated: ', data);
+		}, function(err){
+			console.error(err);
+		});
+	}
+}
+
+loadMarket();
